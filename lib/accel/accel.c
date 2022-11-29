b6c57aac58bf5c0f12d786ae4db0b56160ba1da0
JadedTuna <jadedtuna@gmail.com>
2022-11-29 04:09:19 +0100

    Add more accel/gyro functions and change interface
    Accelerometer breakout allows specifying a range for different
    accuracy requirements. Instead of having to provide it each time
    one of the read functions is called, the selected range will be
    carried by the `cps_accel_t` struct, initialized by `cps_accel_init`.
    This helps prevent against accidentally passing in the wrong range
    when calling a read function, and cleans up the interface.

--- lib/accel/accel.c
+++ lib/accel/accel.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdint.h>
 
@@ -8,8 +9,22 @@
 
 #include "accel.h"
 
-static int cps_accel_scale_mod(cps_accel_range_t range) {
-    return 0x8000 / range;
+static float cps_accel_scale_mod(cps_accel_range_t range) {
+    switch (range) {
+    case ACC_SCALE_2_G   : return 0x4000;
+    case ACC_SCALE_4_G   : return 0x2000;
+    case ACC_SCALE_8_G   : return 0x1000;
+    case ACC_SCALE_16_G  : return 0x0800;
+    }
+}
+
+static float cps_gyro_scale_mod(cps_gyro_range_t range) {
+    switch (range) {
+    case GYRO_SCALE_250_DEG : return 131.0;
+    case GYRO_SCALE_500_DEG : return  65.5;
+    case GYRO_SCALE_1000_DEG: return  32.8;
+    case GYRO_SCALE_2000_DEG: return  16.4;
+    }
 }
 
 cps_err_t cps_i2c_read16(int fd, uint8_t command, uint16_t *result) {
@@ -26,7 +41,7 @@ cps_err_t cps_i2c_read16(int fd, uint8_t command, uint16_t *result) {
 }
 
 cps_err_t cps_accel_init(cps_accel_t *acc, const char *device, int i2c_addr,
-        cps_accel_range_t range) {
+        cps_accel_range_t accel_range, cps_gyro_range_t gyro_range) {
     int fd;
 
     if ((fd = open(device, O_RDWR)) < 0) {
@@ -43,24 +58,87 @@ cps_err_t cps_accel_init(cps_accel_t *acc, const char *device, int i2c_addr,
         return CPS_ERR_SYS;
     }
 
-    if (i2c_smbus_write_word_data(fd, ACC_CONFIG, range) < 0) {
+    /* setup accelerometer and gyroscope ranges */
+    if (i2c_smbus_write_word_data(fd, ACC_CONFIG, accel_range) < 0) {
+        return CPS_ERR_SYS;
+    }
+    if (i2c_smbus_write_word_data(fd, GYRO_CONFIG, gyro_range) < 0) {
         return CPS_ERR_SYS;
     }
 
     acc->fd = fd;
+    acc->accel_range = accel_range;
+    acc->gyro_range = gyro_range;
 
     return CPS_ERR_OK;
 }
 
 cps_err_t cps_accel_read_accel(cps_accel_t *acc, cps_accel_dir_t dir,
-        cps_accel_range_t range, float *result) {
+        float *result) {
     int16_t data;
+    uint8_t cmd;
+
+    switch (dir) {
+    case ACC_DIR_X: cmd = 0x3B; break;
+    case ACC_DIR_Y: cmd = 0x3D; break;
+    case ACC_DIR_Z: cmd = 0x3F; break;
+    default: return CPS_ERR_ARG;
+    }
 
     if (cps_i2c_read16(acc->fd, dir, &data) < 0) {
         return CPS_ERR_SYS;
     }
 
-    *result = (float)data / cps_accel_scale_mod(range);
+    *result = (float)data / cps_accel_scale_mod(acc->accel_range);
 
     return CPS_ERR_OK;
+}
+
+cps_err_t cps_accel_read_gyro(cps_accel_t *acc, cps_accel_dir_t dir,
+        float *result) {
+    int16_t data;
+    uint8_t cmd;
+
+    switch (dir) {
+    case ACC_DIR_X: cmd = 0x43; break;
+    case ACC_DIR_Y: cmd = 0x45; break;
+    case ACC_DIR_Z: cmd = 0x47; break;
+    default: return CPS_ERR_ARG;
+    }
+
+    if (cps_i2c_read16(acc->fd, cmd, &data) < 0) {
+        return CPS_ERR_SYS;
+    }
+
+    *result = (float)data / cps_gyro_scale_mod(acc->gyro_range);
+
+    return CPS_ERR_OK;
+}
+
+cps_err_t cps_accel_read_angle(cps_accel_t *acc, cps_accel_dir_t axis,
+        float *result) {
+    float data;
+    float angle_rad;
+    cps_err_t ret = CPS_ERR_OK;
+
+    switch (axis) {
+    case ACC_DIR_X: {
+        CPS_RET_ON_ERR(cps_accel_read_accel(acc, ACC_DIR_Y, &data));
+        /* TODO: should this be clamped or scaled? */
+        data = MAX(-1.0, MIN(1.0, data));
+        angle_rad = asin(data);
+        break;
+    }
+    case ACC_DIR_Y: {
+        CPS_RET_ON_ERR(cps_accel_read_accel(acc, ACC_DIR_X, &data));
+        data = MAX(-1.0, MIN(1.0, data));
+        angle_rad = -asin(data);
+        break;
+    }
+    default: return CPS_ERR_ARG;
+    }
+
+    *result = angle_rad * 180/M_PI;
+
+    return ret;
 }
--- lib/accel/accel.h
+++ lib/accel/accel.h
@@ -1,35 +1,54 @@
 #pragma once
 
+#define CPS_RET_ON_ERR(stmt) if ((ret = (stmt)) != CPS_ERR_OK) { return ret; }
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+typedef enum {
+    ACC_SCALE_2_G   = 0 << 3,
+    ACC_SCALE_4_G   = 1 << 3,
+    ACC_SCALE_8_G   = 2 << 3,
+    ACC_SCALE_16_G  = 3 << 3,
+} cps_accel_range_t;
+
+typedef enum {
+    /* scale x1000 */
+    GYRO_SCALE_250_DEG  = 0 << 3,
+    GYRO_SCALE_500_DEG  = 1 << 3,
+    GYRO_SCALE_1000_DEG = 2 << 3,
+    GYRO_SCALE_2000_DEG = 3 << 3,
+} cps_gyro_range_t;
+
 typedef struct {
     int fd;
+    cps_accel_range_t accel_range;
+    cps_gyro_range_t gyro_range;
 } cps_accel_t;
 
 typedef enum {
     CPS_ERR_OK = 0,
     CPS_ERR_FAIL,   /* generic error */
     CPS_ERR_SYS,    /* system error (check errno) */
+    CPS_ERR_ARG,    /* bad argument */
 } cps_err_t;
 
 typedef enum {
-    /* TODO: confirm names for ranges */
-    ACC_RANGE_2  = 0x00,
-    ACC_RANGE_4  = 0x08,
-    ACC_RANGE_8  = 0x10,
-    ACC_RANGE_16 = 0x18,
-} cps_accel_range_t;
-
-typedef enum {
-    ACC_DIR_X = 0x3B,
-    ACC_DIR_Y = 0x3D,
-    ACC_DIR_Z = 0x3F,
+    ACC_DIR_X,
+    ACC_DIR_Y,
+    ACC_DIR_Z,
 } cps_accel_dir_t;
 
 typedef enum {
     ACC_CONFIG      = 0x1C,
+    GYRO_CONFIG     = 0x1B,
     ACC_PWR_MGMT_1  = 0x6B,
-} cps_acc_cmd_t;
+} cps_accel_cmd_t;
 
 cps_err_t cps_accel_init(cps_accel_t *acc, const char *device, int i2c_addr,
-    cps_accel_range_t range);
+    cps_accel_range_t accel_range, cps_gyro_range_t gyro_range);
 cps_err_t cps_accel_read_accel(cps_accel_t *acc, cps_accel_dir_t dir,
-        cps_accel_range_t range, float *result);
+    float *result);
+cps_err_t cps_accel_read_gyro(cps_accel_t *acc, cps_accel_dir_t dir,
+    float *result);
+cps_err_t cps_accel_read_angle(cps_accel_t *acc, cps_accel_dir_t axis,
+    float *result);

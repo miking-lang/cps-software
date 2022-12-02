9455eccae9ea6966db58cbfb3a79a0482426aeb5
JadedTuna <jadedtuna@gmail.com>
2022-12-02 18:51:29 +0100

    Add required libraries to CMakeLists, fix accel read typo

--- lib/accel/accel.c
+++ lib/accel/accel.c
@@ -27,16 +27,25 @@ static float cps_gyro_scale_mod(cps_gyro_range_t range) {
     }
 }
 
-cps_err_t cps_i2c_read16(int fd, uint8_t command, uint16_t *result) {
+cps_err_t cps_i2c_read16(int fd, uint8_t reg, uint16_t *result) {
     int32_t data;
 
-    data = i2c_smbus_read_word_data(fd, command);
+    data = i2c_smbus_read_word_data(fd, reg);
     if (data < 0)
         return CPS_ERR_SYS;
     /* byteswap */
     data = ((data & 0xFF) << 8) | ((data & 0xFF00) >> 8);
 
     *result = (int16_t)data;
+    return CPS_ERR_OK;
+}
+
+cps_err_t cps_i2c_write16(int fd, uint8_t reg, uint16_t data) {
+    /* write 2 bytes */
+    if (i2c_smbus_write_word_data(fd, reg, data) != 2) {
+        return CPS_ERR_SYS;
+    }
+
     return CPS_ERR_OK;
 }
 
@@ -53,18 +62,12 @@ cps_err_t cps_accel_init(cps_accel_t *acc, const char *device, int i2c_addr,
         return CPS_ERR_SYS;
     }
 
-    if (i2c_smbus_write_word_data(fd, ACC_PWR_MGMT_1, 0) < 0) {
-        /* check address/connection */
-        return CPS_ERR_SYS;
-    }
+    /* on error: check address/connection */
+    CPS_RET_ON_ERR(cps_i2c_write16(fd, ACC_PWR_MGMT_1, 0));
 
     /* setup accelerometer and gyroscope ranges */
-    if (i2c_smbus_write_word_data(fd, ACC_CONFIG, accel_range) < 0) {
-        return CPS_ERR_SYS;
-    }
-    if (i2c_smbus_write_word_data(fd, GYRO_CONFIG, gyro_range) < 0) {
-        return CPS_ERR_SYS;
-    }
+    CPS_RET_ON_ERR(cps_i2c_write16(fd, ACC_CONFIG, accel_range));
+    CPS_RET_ON_ERR(cps_i2c_write16(fd, GYRO_CONFIG, gyro_range));
 
     acc->fd = fd;
     acc->accel_range = accel_range;
@@ -76,16 +79,16 @@ cps_err_t cps_accel_init(cps_accel_t *acc, const char *device, int i2c_addr,
 cps_err_t cps_accel_read_accel(cps_accel_t *acc, cps_accel_dir_t dir,
         float *result) {
     int16_t data;
-    uint8_t cmd;
+    uint8_t reg;
 
     switch (dir) {
-    case ACC_DIR_X: cmd = 0x3B; break;
-    case ACC_DIR_Y: cmd = 0x3D; break;
-    case ACC_DIR_Z: cmd = 0x3F; break;
+    case ACC_DIR_X: reg = 0x3B; break;
+    case ACC_DIR_Y: reg = 0x3D; break;
+    case ACC_DIR_Z: reg = 0x3F; break;
     default: return CPS_ERR_ARG;
     }
 
-    if (cps_i2c_read16(acc->fd, dir, &data) < 0) {
+    if (cps_i2c_read16(acc->fd, reg, &data) < 0) {
         return CPS_ERR_SYS;
     }
 
@@ -97,16 +100,16 @@ cps_err_t cps_accel_read_accel(cps_accel_t *acc, cps_accel_dir_t dir,
 cps_err_t cps_accel_read_gyro(cps_accel_t *acc, cps_accel_dir_t dir,
         float *result) {
     int16_t data;
-    uint8_t cmd;
+    uint8_t reg;
 
     switch (dir) {
-    case ACC_DIR_X: cmd = 0x43; break;
-    case ACC_DIR_Y: cmd = 0x45; break;
-    case ACC_DIR_Z: cmd = 0x47; break;
+    case ACC_DIR_X: reg = 0x43; break;
+    case ACC_DIR_Y: reg = 0x45; break;
+    case ACC_DIR_Z: reg = 0x47; break;
     default: return CPS_ERR_ARG;
     }
 
-    if (cps_i2c_read16(acc->fd, cmd, &data) < 0) {
+    if (cps_i2c_read16(acc->fd, reg, &data) < 0) {
         return CPS_ERR_SYS;
     }
 

60062da77b0f7732d760c377e326864a692763b2
John Wikman <john.wikman@hotmail.com>
2024-03-04 14:10:44 +0100

    Update docker image with right dependencies

--- remote-ctrl/server/server_spider.py
+++ remote-ctrl/server/server_spider.py
@@ -4,4 +4,4 @@ import cps_server
 
 if __name__ == "__main__":
     #cps_server.connection.run_spider()
-    cps_server.run_policy.run_policy("/mnt/model.zip")    
+    cps_server.run_policy.run_policy("/mnt/model")

acf5fef6b2f3ade09656795536f809bbdbc678a8
Oscar Eriksson <oerikss@kth.se>
2024-03-07 16:58:17 +0100

    Add stable planner

--- remote-ctrl/server/cps_server/controllers/spider.py
+++ remote-ctrl/server/cps_server/controllers/spider.py
@@ -1,3 +1,4 @@
+import numpy as np
 from .controllerbase import ControllerBase, register_command
 
 SERVO_INDEX_LOOKUP = {
@@ -50,6 +51,7 @@ class SpiderController(ControllerBase):
 
         self.duration = 1500
         self.acceleration = 600
+        self.velocity_max = np.pi / 4
         self.MIN_DURATION = 100
         self.MIN_ACCELERATION = self.MIN_DURATION // 2
 
@@ -190,6 +192,57 @@ class SpiderController(ControllerBase):
             acceleration=self.acceleration,
         )
         return dict()
+
+
+    @register_write(argtypes=[int]*len(ALL_SERVO_IDS))
+    def move_all_servos_steady(self, *positions):
+        for v in positions:
+            if v not in range(0, 4096):
+                raise ValueError(f"Servo values must be in range 0 to 4095, got value {v}")
+
+        data = self.read_all_servo_planned_positions_velocities()
+
+        positions_planned = data["POSITION_TRAJECTORY"]
+        velocities_planned = data["VELOCITY_TRAJECTORY"]
+
+        t1 = self.acceleration
+
+        # Convert the maximum velocity to dynamixel units
+        velocity_max = round(self.velocity_max * (4096 / (2*np.pi)))
+
+
+        positions_deltas = [
+            pos - pos_plan
+            for pos, pos_plan in zip(positions, positions_planned)
+        ]
+
+        velocities = [
+            min((pos_delta - 0.05*vel_plan*t1) / (2*t1), velocity_max)
+            for pos_delta, vel_plan in zip(positions_deltas, velocities_planned)
+        ]
+
+        durations = [
+            (pos_delta - 0.5*vel_plan*t1) / vel + t1
+            for pos_delta, vel, vel_plan in
+            zip(positions_deltas, velocities, velocities_planned)
+        ]
+
+        self.dxl_handler.position_control(
+            ALL_SERVO_IDS,
+            positions,
+            duration=durations,
+            acceleration=t1,
+        )
+
+        return dict()
+
+    @register_read(argtypes=[])
+    def read_all_servo_planned_positions_velocities(self):
+        return self.dxl_handler.sync_read_registers(
+            ALL_SERVO_IDS,
+            reg_start="VELOCITY_TRAJECTORY",
+            reg_end="POSITION_TRAJECTORY",
+        )
 
     @register_write(argtypes=[str, int])
     def move_single_servo(self, name, position):

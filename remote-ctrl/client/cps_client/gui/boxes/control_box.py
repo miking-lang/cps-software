b4bef4263a026137a4e87a80392d6b96f1788e9b
Oscar Eriksson <oerikss@kth.se>
2024-03-07 17:10:19 +0100

    Convert to integers

--- remote-ctrl/client/cps_client/gui/boxes/control_box.py
+++ remote-ctrl/client/cps_client/gui/boxes/control_box.py
@@ -546,7 +546,7 @@ class ControlBox(Refresher, Gtk.Box):
         if len(self.command_queue) > 0:
             if self.command_received and self.telemetry_received:
                 pos = self.command_queue.popleft()
-                cmd_pkt = slipp.Packet("move_all_servos", contents={"args": copy.copy(pos)})
+                cmd_pkt = slipp.Packet("move_all_servos_steady", contents={"args": copy.copy(pos)})
                 tm_pkt = slipp.Packet("read_all_servos_RAM")
                 self.main_utils.client_send(
                     cmd_pkt,

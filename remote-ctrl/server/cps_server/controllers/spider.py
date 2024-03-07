Fix duration computation

remote-ctrl/server/cps_server/controllers/spider.py
@@ -217,13 +217,12 @@ class SpiderController(ControllerBase):
         ]
 
         velocities = [
-            np.sign(num)*min(abs(num / (2*t1)), velocity_max)
+            np.sign(pos_delta-0.5*vel_plan*t1)*min(abs((pos_delta-0.5*vel_plan*t1) / (2*t1)), velocity_max)
             for pos_delta, vel_plan in zip(positions_deltas, velocities_planned)
-            if (num := pos_delta - 0.5*vel_plan*t1)
         ]
 
         durations = [
-            int(round((pos_delta - 0.5*vel_plan*t1) / vel + t1)) if vel != 0 else self.duration
+            int(round(1000*((pos_delta - 0.5*vel_plan*t1) / vel + t1))) if vel != 0 else self.duration
             for pos_delta, vel, vel_plan in
             zip(positions_deltas, velocities, velocities_planned)
         ]